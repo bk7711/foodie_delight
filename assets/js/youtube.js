6608f32a36aa9d851f677ede6e1bdc804d8d49b9
Carolyn B <bmathematics@gmail.com>
2022-02-14 16:57:19 -0500

    add modal to html and starter code in js

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -2,11 +2,12 @@ var resultsEl = document.querySelector(".results");
 var buttonEl = document.querySelector(".btn");
 var searchEl = document.querySelector("#search");
 var buttonEl = document.querySelector(".btn");
-// var search = "";
-var apiUrl = "";
-var apiUrl = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=24&q=" + searchEl.value + "&type=video&key=AIzaSyCH8PUHPaF-bU95k7xnQcjjG9U740oLEI8";
-;
 
+
+var apiUrl = "";
+var apiUrl = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&order=rating&maxResults=24&q=" + searchEl.value + "&type=video&key=AIzaSyCH8PUHPaF-bU95k7xnQcjjG9U740oLEI8";
+;
+//get and display api results
 var getVideo = function(){
     fetch(apiUrl).then(function(response){
         if(response.ok){
@@ -15,12 +16,13 @@ var getVideo = function(){
                 var videos = data.items
                 console.log(videos);
                 for(var i = 0; i<videos.length; i++){
-                    var videoEl = document .createElement('div');
+                    var videoEl = document.createElement('div');
+                    videoEl.setAttribute("id", videos[i].id.videoId);
+                    videoEl.setAttribute("class", "card");
                     videoEl.innerHTML = `
-                        <div class="card">
-                        <a href="https://www.youtube.com/watch?v=${videos[i].id.videoId}"><img width="300" height="200" src="${videos[i].snippet.thumbnails.medium.url}" alt="${videos[i].snippet.description}"></a>
+                    <a href="https://www.youtube.com/watch?v=${videos[i].id.videoId}" target="_blank"><img width="300" height="200" src="${videos[i].snippet.thumbnails.medium.url}" alt="${videos[i].snippet.description}"></a>
                         <p class="card-title"><a target= "_blank" href="https://www.youtube.com/watch?v=${videos[i].id.videoId}">${videos[i].snippet.title}</a></p>
-                        </div>`;
+                        `;
                     resultsEl.appendChild(videoEl);
                 }
             })
@@ -30,6 +32,7 @@ var getVideo = function(){
     })
 };
 
+//get value of search field
 var searchItem = function(){
     
     var search = searchEl.value;
@@ -38,11 +41,21 @@ var searchItem = function(){
     apiUrl = newUrl
 }
 
+//when search button is clicked, display api results
 buttonEl.addEventListener("click", function(){
     searchItem();
-    
     getVideo();
     // search = "";
 });
 
 
+// $(resultsEl).on("click", function(event){
+//     var targetUrl = $(event.target).parent(["<div>"]).attr("id");
+//     var src = "https://www.youtube.com/watch?v=" + targetUrl + "autoplay=1";
+//     console.log(src);
+//     $("#myModal").modal('show');
+//     $("#myModal iframe").attr('src',src);
+// });  
+// $("#myModal button").click(function(){
+
+// })
